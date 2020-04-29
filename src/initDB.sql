4d162f9d58c175145e612ed9cc77a4da78099bf7
jakechvatal <jakechvatal@github.com>
2020-04-28 23:10:39 -0400

    - safer database creation - autogen keys - enforces uniqueness constraints

--- src/initDB.sql
+++ src/initDB.sql
@@ -1,12 +1,13 @@
 PRAGMA foreign_keys = ON;
 
-CREATE TABLE crawls (
-    crawl_id INTEGER PRIMARY KEY,
-    name TEXT NOT NULL
+CREATE TABLE IF NOT EXISTS crawls (
+    crawl_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    UNIQUE(crawl_id, name) ON CONFLICT ABORT -- if a crawl with a duplicate name is inserted, this fails
 );
 
-CREATE TABLE clues (
-    clue_id INTEGER PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS clues (
+    clue_id INTEGER PRIMARY KEY AUTOINCREMENT,
     crawl_id INTEGER,
     name TEXT NOT NULL,
     place TEXT NOT NULL,
@@ -15,19 +16,23 @@ CREATE TABLE clues (
 
     FOREIGN KEY (crawl_id)
         REFERENCES crawls (crawl_id)
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION,
+
+    UNIQUE(clue_id, name) ON CONFLICT ABORT -- clues cannot have duplicate names or ids
 );
 
+CREATE TABLE IF NOT EXISTS paths (
+    path_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
 
-CREATE TABLE paths (
-    path_id INTEGER PRIMARY KEY,
-    name TEXT NOT NULL
+    UNIQUE(path_id, name) ON CONFLICT ABORT -- paths cannot have duplicate names or ids
 );
-
 
 -- represents a many to many relationship between paths and clues,
 -- allowing us to store a list of paths associated with a clue or a list 
 -- of clues associated with a path
-CREATE TABLE paths_join_clues (
+CREATE TABLE IF NOT EXISTS paths_join_clues (
     path_id INTEGER,
     clue_id INTEGER,
 
