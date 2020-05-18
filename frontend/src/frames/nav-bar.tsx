c98e13c1a353f63bbc3cffd41b843b0cf0ac62cc
ryan <hart.ry@husky.neu.edu>
2020-05-18 17:39:12 -0400

    added dark/light mode toggle

--- frontend/src/frames/nav-bar.tsx
+++ frontend/src/frames/nav-bar.tsx
@@ -9,6 +9,14 @@ interface NavBarFrameState {
 
 }
 
+function switchTheme(checked: boolean) {
+    if (checked) {
+        document.documentElement.setAttribute('data-theme', 'light');
+    } else {
+        document.documentElement.setAttribute('data-theme', 'dark');
+    }
+}
+
 
 export default class NavBarFrame extends React.Component<NavBarFrameProps, NavBarFrameState> {
     constructor(props: NavBarFrameProps) {
@@ -19,7 +27,17 @@ export default class NavBarFrame extends React.Component<NavBarFrameProps, NavBa
 
     render() {
         return (
-            <div className="nav-bar"><a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Navigation Bar</a></div>
-            )
-        }
+            <div className="nav-bar">
+                <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Navigation Bar</a>
+                <div className="theme-switch-wrapper">
+                    <label className="theme-switch" htmlFor="checkbox" >
+                        <input type="checkbox" id="checkbox"
+                            onClick={(event: React.MouseEvent) => switchTheme((event.target as HTMLInputElement).checked)} />
+                        <div className="slider round"></div>
+                    </label>
+                    <em>Enable Light Mode!</em>
+                </div>
+            </div>
+        )
+    }
 }
