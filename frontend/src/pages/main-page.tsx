4c8d581a5752e48a62ad2cd27404345b3510924e
ryan <hart.ry@husky.neu.edu>
2020-05-19 16:24:01 -0400

    Moved nav-bar css out of main-page file

--- frontend/src/pages/main-page.tsx
+++ frontend/src/pages/main-page.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ClueFrame from "../frames/clue-frame";
 import "../css/main-page.css";
+import "../css/nav-bar.css"
 import NavBarFrame from "../frames/nav-bar";
 import PathFrame from "../frames/path-frame";
 import GroupFrame from "../frames/group-frame";
