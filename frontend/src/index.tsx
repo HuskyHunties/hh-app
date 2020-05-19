fec66a0750e0190c7668fb54707a18564c5b3f61
ryan <hart.ry@husky.neu.edu>
2020-05-19 16:22:14 -0400

    fixed minor merge errors

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,16 +1,8 @@
-<<<<<<< HEAD
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 import MainPage from "./pages/main-page";
-=======
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
->>>>>>> backendwork
 
 ReactDOM.render(
   <React.StrictMode>
