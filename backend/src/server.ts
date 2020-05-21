8919ffaede28bd95c04fae74d52b6131598b93a2
Michael Zappa <zapprich@gmail.com>
2020-05-21 14:35:21 -0500

    fixed warnings in server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -7,7 +7,7 @@ const server = http.createServer(Main);
 
 /**
  * Coerce a port number into its integer representation
- * @param val a number or string representing port number
+ * @param val - a number or string representing port number
  */
 function normalizePort(val: number | string): number | string | boolean {
   const port: number = typeof val === "string" ? parseInt(val, 10) : val;
@@ -21,7 +21,7 @@ debug("ts-express:server");
 
 /**
  * Event handler for error events in the ExpressJS server.
- * @param error NodeJS error, typically thrown by Express middleware
+ * @param error - NodeJS error, typically thrown by Express middleware
  */
 function onError(error: NodeJS.ErrnoException): void {
   if (error.syscall !== "listen") throw error;
