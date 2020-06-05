99645ebfc2ba173f67a97700b8f67474f9801209
ryan <hart.ry@husky.neu.edu>
2020-06-05 18:49:58 -0400

    changed API to use production backend url

--- frontend/src/utils/API.ts
+++ frontend/src/utils/API.ts
@@ -5,6 +5,6 @@ axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
  * Creates the default axios object to access the backend.
  */
 export default axios.create({
-    baseURL: "http://localhost:3000/",
+    baseURL: "https://husky-hunties.azurewebsites.net",
     responseType: "json"
 });
