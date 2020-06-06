ad3afdd682e9b3f1b0a0ff2dc330539eef5796fe
Michael Zappa <zapprich@gmail.com>
2020-06-06 09:28:47 -0500

    connected mobile to azure

--- mobile/utils/API.ts
+++ mobile/utils/API.ts
@@ -5,6 +5,6 @@ axios.defaults.headers.post["Access-Control-Allow-Origin"] = "*";
  * Creates the default axios object to access the backend.
  */
 export default axios.create({
-  baseURL: "http://10.0.0.159:3000",
+  baseURL: "https://husky-hunties.azurewebsites.net/",
   responseType: "json",
 });
