ccf60b5dd5043c5e79ff4a50826bd9e7423a7831
michzappa <zapprich@gmail.com>
2020-06-24 22:33:48 -0500

    updated ip

--- frontend/src/utils/API.ts
+++ frontend/src/utils/API.ts
@@ -5,6 +5,6 @@ axios.defaults.headers.post["Access-Control-Allow-Origin"] = "*";
  * Creates the default axios object to access the backend.
  */
 export default axios.create({
-  baseURL: "http://35.245.207.244:3000/",
+  baseURL: "http://34.86.145.222:3000/",
   responseType: "json",
 });
