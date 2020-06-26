06f0360e59e3862bc567f7402d141c6b3b9575d9
ryan <hart.ry@husky.neu.edu>
2020-06-26 17:41:30 -0400

    frontend build files will now be tracked by git

---
+++ frontend/build/precache-manifest.35ed3392efb332a1797351bdc9ee3cf2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf4007e67cb077fed60e06750ff54181",
+    "url": "/hh-app/index.html"
+  },
+  {
+    "revision": "393fa1a706cf8f68c602",
+    "url": "/hh-app/static/css/main.bda39611.chunk.css"
+  },
+  {
+    "revision": "bd41fc2fe15e0298dd4e",
+    "url": "/hh-app/static/js/2.60ec2cb2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hh-app/static/js/2.60ec2cb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "393fa1a706cf8f68c602",
+    "url": "/hh-app/static/js/main.e32eee8b.chunk.js"
+  },
+  {
+    "revision": "3fc3d4a6dff98282f5e4",
+    "url": "/hh-app/static/js/runtime-main.579f0892.js"
+  }
+]);
