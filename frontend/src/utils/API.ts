8af3054ac46b2194f4c02a55fc1b837f53f03cb8
michzappa <zapprich@gmail.com>
2020-06-26 20:36:47 -0500

    added google vm to the axios link

--- frontend/src/utils/API.ts
+++ frontend/src/utils/API.ts
@@ -5,7 +5,7 @@ axios.defaults.headers.post["Access-Control-Allow-Origin"] = "*";
  * Creates the default axios object to access the backend.
  */
 export default axios.create({
-  baseURL: "https://husky-hunties.azurewebsites.net",
-  //baseURL: "http://localhost:3000",
+  //baseURL: "https://husky-hunties.azurewebsites.net",
+  baseURL: "http://34.86.145.222:3000",
   responseType: "json",
 });
